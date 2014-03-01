28be41585e3ec90a2fbcb5bcd45d12434d7b78cc
ScarWu <xneriscool@gmail.com>
2014-03-01 23:02:23 +0800

    follow PSR-0 PSR-1 PSR-2

--- example/example.php
+++ example/example.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Example
- * 
+ *
  * @package		Element
  * @author		ScarWu
- * @copyright	Copyright (c) 2012, ScarWu (http://scar.simcz.tw/)
+ * @copyright	Copyright (c) 2012-2014, ScarWu (http://scar.simcz.tw/)
  * @link		http://github.com/scarwu/Element
  */
 
-require_once realpath(__DIR__ . '/../Element.php');
+require_once realpath(__DIR__ . '/../src/Element.php');
 
 // New Element
 $_ = new Element();
@@ -28,38 +28,38 @@ echo $_->tag('img')->set('src', '/path/to/img')->single()."\n";
 
 // create tag a with some attribute
 // <a href="http://scar.simcz.tw" target="_blank">ScarShow</a>
-echo $_->tag('a')->set(array(
-	'href' => 'http://scar.simcz.tw',
-	'target' => '_blank'
-))->add('ScarShow')."\n";
+echo $_->tag('a')->set([
+    'href' => 'http://scar.simcz.tw',
+    'target' => '_blank'
+])->add('ScarShow')."\n";
 
 // create div with some content
 // if you want to get element result then use function str() and at the last
 // <div id="main"><span>Hello, My blog is </span><a href="http://scar.simcz.tw" target="_blank">ScarShow</a></div>
 echo $_->tag('div')->set('id', 'main')->add(
-	'Hi, My blog is ',
-	$_->tag('a')->set(array(
-		'href' => 'http://scar.simcz.tw',
-		'target' => '_blank'
-	))->add('ScarShow')->str()
+    'Hi, My blog is ',
+    $_->tag('a')->set([
+        'href' => 'http://scar.simcz.tw',
+        'target' => '_blank'
+    ])->add('ScarShow')->str()
 )."\n";
 
 /**
  * Simple Function
  */
-	
+
 // <div id="main"></div>
 echo $_('div')->id('main')."\n";
 
 // <img src="/path/to/img" />
-echo $_('img', array('src' => '/path/to/img'))->single()."\n";
+echo $_('img', ['src' => '/path/to/img'])->single()."\n";
 // or
 echo $_->img('/path/to/img')."\n";
 
 // <a href="http://scar.simcz.tw" target="_blank">ScarShow</a>
-echo $_('a', array(
-	'href' => 'http://scar.simcz.tw',
-	'target' => '_blank'
-), 'ScarShow')."\n";
+echo $_('a', [
+    'href' => 'http://scar.simcz.tw',
+    'target' => '_blank'
+], 'ScarShow')."\n";
 // or
 echo $_->a('http://scar.simcz.tw', '_blank', 'ScarShow')."\n";
